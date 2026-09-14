a17db0350ca5f48bb6120bcd1ffc381c65049568
stran <bocay113@gmail.com>
2026-09-14 19:55:31 +0700

    Add CI/CD for Frontend

---
+++ webui/eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores(['**/dist', '**/.nx', '**/node_modules']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+    ],
+  },
+]);
